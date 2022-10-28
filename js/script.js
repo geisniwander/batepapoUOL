3056cf258d25da86b321445c6e1488e8752ddcd6
geisniwander <geisniwander@gmail.com>
2022-10-28 15:37:46 -0300

    tratamento-mensagens

--- js/script.js
+++ js/script.js
@@ -1,33 +1,55 @@
 const chat = document.querySelector(".chat");
+let user={name:''};
 
-
-function enviarMensagem(){
-    const mensagem = document.querySelector(".mensagem").value;
-    alert(mensagem);
+function registerUser(){
+    user.name = prompt("Digite seu nome");
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',user);
+    promise.then(getMessage);
+    promise.catch(registerUser);
 }
 
-function buscarMensagem(){
+function keepConnected(){
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status',user);
+    promise.then(getMessage);
+    promise.catch(registerUser);
+}
+
+function sendMessage(){
+    const message = document.querySelector(".textMessage").value;
+    const messageObj = {from:user.name, to:"Todos",text:message,type:"message"};
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages',messageObj);
+    promise.then(getMessage);
+    promise.catch(registerUser);
+}
+
+function getMessage(){
     const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    promise.then(renderizaMensagem,promise);
+    promise.then(renderMessages,promise);
 }
 
-function renderizaMensagem(novasMensagens){
-    const objetos = novasMensagens.data;
+function renderMessages(newMessages){
+    const objects = newMessages.data;
+    chat.innerHTML="";
+    for(let i=1; i<objects.length; i++){
+        let message = {from: objects[i].from, to: objects[i].to, text: objects[i].text, type:objects[i].type, time:objects[i].time}
 
-    for(let i=0; i<objetos.length; i++){
-        let mensagem = {from: objetos[i].from, to: objetos[i].to, text: objetos[i].text, type:objetos[i].type, time:objetos[i].time}
-
-        if(mensagem.type==="status"){
-            chat.innerHTML += `<li>(${mensagem.time}) <span>${mensagem.from}</span> ${mensagem.text}</li>`;
-
+        if(message.type==="status"){
+            const msg = (`<li class="status msg${i}">(${message.time}) <span>${message.from}</span> ${message.text}</li>`);
+            chat.innerHTML+= msg;
         }
-        else if(mensagem.type==="message"){
-            chat.innerHTML += `<li>(${mensagem.time}) <span>${mensagem.from}</span> para <span>${mensagem.to}:</span> ${mensagem.text}</li>`;
-        }
+        else if(message.type==="message" & message.to==="Todos"){
+            const msg =(`<li class="message msg${i}">(${message.time}) <span>${message.from}</span> para <span>${message.to}:</span> ${message.text}</li>`);
+            chat.innerHTML+=msg;
+            }
         else{
-            chat.innerHTML += `<li class="privada">(${mensagem.time}) <span>${mensagem.from}</span> para <span>${mensagem.to}:</span> ${mensagem.text}</li>`;
+            const msg =(`<li class="private msg${i}">(${message.time}) <span>${message.from}</span> para <span>${message.to}:</span> ${message.text}</li>`);
+            chat.innerHTML+= msg;
         }
+        const last = document.querySelector(`.msg${i}`);
+        last.scrollIntoView();
     }
 }
 
-buscarMensagem();
+registerUser();
+setInterval(keepConnected,3000);
+
